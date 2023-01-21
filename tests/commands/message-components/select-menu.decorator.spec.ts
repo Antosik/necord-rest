3df3b7d86706a7386d9de8540c2536a7bcd00046
Antosik <grig_anton96@mail.ru>
2023-01-21 18:06:33 +0300

    test: add tests for message-components

---
+++ tests/commands/message-components/select-menu.decorator.spec.ts
@@ -0,0 +1,139 @@
+import { Injectable } from '@nestjs/common';
+import { ComponentType } from 'discord-api-types/v10';
+
+import {
+	ChannelSelect,
+	MentionableSelect,
+	RoleSelect,
+	StringSelect,
+	UserSelect
+} from '../../../src';
+import { MessageComponentMeta } from '../../../src/commands/message-components/message-component.interface';
+import { MESSAGE_COMPONENT_METADATA } from '../../../src/necord.constants';
+
+describe('Message Components > Select Menu Decorator', () => {
+	describe('StringSelect', () => {
+		const meta: Omit<MessageComponentMeta, 'type'> = {
+			customId: 'stringSelect'
+		};
+
+		@Injectable()
+		class DiscordService {
+			@StringSelect(meta.customId)
+			static menu() {
+				return 'ok';
+			}
+		}
+
+		it('should set message command meta', () => {
+			const metaFromDecorator = Reflect.getMetadata(
+				MESSAGE_COMPONENT_METADATA,
+				DiscordService.menu
+			);
+			expect(metaFromDecorator.meta).toEqual({
+				...meta,
+				type: ComponentType.StringSelect
+			});
+		});
+	});
+
+	describe('ChannelSelect', () => {
+		const meta: Omit<MessageComponentMeta, 'type'> = {
+			customId: 'channelSelect'
+		};
+
+		@Injectable()
+		class DiscordService {
+			@ChannelSelect(meta.customId)
+			static menu() {
+				return 'ok';
+			}
+		}
+
+		it('should set message command meta', () => {
+			const metaFromDecorator = Reflect.getMetadata(
+				MESSAGE_COMPONENT_METADATA,
+				DiscordService.menu
+			);
+			expect(metaFromDecorator.meta).toEqual({
+				...meta,
+				type: ComponentType.ChannelSelect
+			});
+		});
+	});
+
+	describe('UserSelect', () => {
+		const meta: Omit<MessageComponentMeta, 'type'> = {
+			customId: 'userSelect'
+		};
+
+		@Injectable()
+		class DiscordService {
+			@UserSelect(meta.customId)
+			static menu() {
+				return 'ok';
+			}
+		}
+
+		it('should set message command meta', () => {
+			const metaFromDecorator = Reflect.getMetadata(
+				MESSAGE_COMPONENT_METADATA,
+				DiscordService.menu
+			);
+			expect(metaFromDecorator.meta).toEqual({
+				...meta,
+				type: ComponentType.UserSelect
+			});
+		});
+	});
+
+	describe('MentionableSelect', () => {
+		const meta: Omit<MessageComponentMeta, 'type'> = {
+			customId: 'mentionableSelect'
+		};
+
+		@Injectable()
+		class DiscordService {
+			@MentionableSelect(meta.customId)
+			static menu() {
+				return 'ok';
+			}
+		}
+
+		it('should set message command meta', () => {
+			const metaFromDecorator = Reflect.getMetadata(
+				MESSAGE_COMPONENT_METADATA,
+				DiscordService.menu
+			);
+			expect(metaFromDecorator.meta).toEqual({
+				...meta,
+				type: ComponentType.MentionableSelect
+			});
+		});
+	});
+
+	describe('RoleSelect', () => {
+		const meta: Omit<MessageComponentMeta, 'type'> = {
+			customId: 'roleSelect'
+		};
+
+		@Injectable()
+		class DiscordService {
+			@RoleSelect(meta.customId)
+			static menu() {
+				return 'ok';
+			}
+		}
+
+		it('should set message command meta', () => {
+			const metaFromDecorator = Reflect.getMetadata(
+				MESSAGE_COMPONENT_METADATA,
+				DiscordService.menu
+			);
+			expect(metaFromDecorator.meta).toEqual({
+				...meta,
+				type: ComponentType.RoleSelect
+			});
+		});
+	});
+});
--- tests/utils/interactions.ts
+++ tests/utils/interactions.ts
@@ -1,29 +1,45 @@
 import {
+	APIApplicationCommandInteraction,
 	APIChatInputApplicationCommandInteraction,
+	APIInteraction,
+	APIMessage,
 	APIMessageApplicationCommandInteraction,
+	APIMessageComponentButtonInteraction,
+	APIMessageComponentInteraction,
+	APIMessageComponentSelectMenuInteraction,
 	APIPingInteraction,
 	APIUserApplicationCommandInteraction,
 	ApplicationCommandType,
-	InteractionType
+	ComponentType,
+	InteractionType,
+	MessageType
 } from 'discord-api-types/v10';
 
-export const pingInteraction: APIPingInteraction = {
-	type: InteractionType.Ping,
+const partialInteraction: Pick<APIInteraction, 'id' | 'application_id' | 'token' | 'version'> = {
 	id: '1',
 	application_id: '1',
 	token: 'token',
 	version: 1
 };
 
-export const chatInputApplicationCommandInteraction: APIChatInputApplicationCommandInteraction = {
+export const pingInteraction: APIPingInteraction = {
+	...partialInteraction,
+	type: InteractionType.Ping
+};
+
+const partialApplicationCommandInteraction: Pick<
+	APIApplicationCommandInteraction,
+	'type' | 'locale' | 'channel_id' | 'app_permissions'
+> = {
 	type: InteractionType.ApplicationCommand,
-	id: '1',
-	application_id: '1',
-	token: 'token',
-	version: 1,
 	locale: 'en-US',
 	channel_id: '1',
-	app_permissions: '1',
+	app_permissions: '1'
+};
+
+export const chatInputApplicationCommandInteraction: APIChatInputApplicationCommandInteraction = {
+	...partialInteraction,
+	...partialApplicationCommandInteraction,
 	data: {
 		type: ApplicationCommandType.ChatInput,
 		id: '1',
@@ -32,14 +48,8 @@ export const chatInputApplicationCommandInteraction: APIChatInputApplicationComm
 };
 
 export const userApplicationCommandInteraction: APIUserApplicationCommandInteraction = {
-	type: InteractionType.ApplicationCommand,
-	id: '1',
-	application_id: '1',
-	token: 'token',
-	version: 1,
-	locale: 'en-US',
-	channel_id: '1',
-	app_permissions: '1',
+	...partialInteraction,
+	...partialApplicationCommandInteraction,
 	data: {
 		type: ApplicationCommandType.User,
 		id: '1',
@@ -52,14 +62,8 @@ export const userApplicationCommandInteraction: APIUserApplicationCommandInterac
 };
 
 export const messageApplicationCommandInteraction: APIMessageApplicationCommandInteraction = {
-	type: InteractionType.ApplicationCommand,
-	id: '1',
-	application_id: '1',
-	token: 'token',
-	version: 1,
-	locale: 'en-US',
-	channel_id: '1',
-	app_permissions: '1',
+	...partialInteraction,
+	...partialApplicationCommandInteraction,
 	data: {
 		type: ApplicationCommandType.Message,
 		id: '1',
@@ -70,3 +74,112 @@ export const messageApplicationCommandInteraction: APIMessageApplicationCommandI
 		}
 	}
 };
+
+const message: APIMessage = {
+	id: '1',
+	channel_id: '1',
+	author: {
+		id: '1',
+		username: '1',
+		discriminator: '1',
+		avatar: '1'
+	},
+	content: '1',
+	timestamp: Date.now().toString(),
+	edited_timestamp: Date.now().toString(),
+	tts: false,
+	mention_everyone: false,
+	mentions: [],
+	mention_roles: [],
+	attachments: [],
+	embeds: [],
+	pinned: false,
+	type: MessageType.Default
+};
+
+const partialMessageComponentInteraction: Pick<APIMessageComponentInteraction, 'type' | 'message'> =
+	{
+		type: InteractionType.MessageComponent,
+		message
+	};
+
+export const messageComponentButtonInteraction: APIMessageComponentButtonInteraction = {
+	...partialInteraction,
+	...partialApplicationCommandInteraction,
+	...partialMessageComponentInteraction,
+	data: {
+		custom_id: '1',
+		component_type: ComponentType.Button
+	}
+};
+
+export const messageComponentStringSelectMenuInteraction: APIMessageComponentSelectMenuInteraction =
+	{
+		...partialInteraction,
+		...partialApplicationCommandInteraction,
+		...partialMessageComponentInteraction,
+		data: {
+			custom_id: '1',
+			component_type: ComponentType.StringSelect,
+			values: []
+		}
+	};
+
+export const messageComponentChannelSelectMenuInteraction: APIMessageComponentSelectMenuInteraction =
+	{
+		...partialInteraction,
+		...partialApplicationCommandInteraction,
+		...partialMessageComponentInteraction,
+		data: {
+			custom_id: '1',
+			component_type: ComponentType.ChannelSelect,
+			values: [],
+			resolved: {
+				channels: {}
+			}
+		}
+	};
+
+export const messageComponentUserSelectMenuInteraction: APIMessageComponentSelectMenuInteraction = {
+	...partialInteraction,
+	...partialApplicationCommandInteraction,
+	...partialMessageComponentInteraction,
+	data: {
+		custom_id: '1',
+		component_type: ComponentType.UserSelect,
+		values: [],
+		resolved: {
+			users: {}
+		}
+	}
+};
+
+export const messageComponentRoleSelectMenuInteraction: APIMessageComponentSelectMenuInteraction = {
+	...partialInteraction,
+	...partialApplicationCommandInteraction,
+	...partialMessageComponentInteraction,
+	data: {
+		custom_id: '1',
+		component_type: ComponentType.RoleSelect,
+		values: [],
+		resolved: {
+			roles: {}
+		}
+	}
+};
+
+export const messageComponentMentionableSelectMenuInteraction: APIMessageComponentSelectMenuInteraction =
+	{
+		...partialInteraction,
+		...partialApplicationCommandInteraction,
+		...partialMessageComponentInteraction,
+		data: {
+			custom_id: '1',
+			component_type: ComponentType.MentionableSelect,
+			values: [],
+			resolved: {
+				users: {},
+				roles: {}
+			}
+		}
+	};
